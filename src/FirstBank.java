125514fba0aa2b562b6696f40ccdbdf688bae9fc
Adam Habil <ahabil@outlook.com>
2021-06-11 12:26:27 -0600

    Add files via upload

---
+++ src/FirstBank.java
@@ -0,0 +1,8 @@
+public class FirstBank {
+    public static void main(String[] args) {
+        //TODO auto-generated method stub.
+
+        Account acc = new Account("Lorem Ipsum", "A00001");
+        acc.showMenu();
+    }
+}
