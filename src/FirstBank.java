7979ee374eec66b38c39c9f7ce34f831ae458a93
Adam Habil <ahabil@outlook.com>
2021-06-16 14:57:53 -0600

    Update FirstBank.java
    User now inputs their account name and account ID in order to proceed.

--- src/FirstBank.java
+++ src/FirstBank.java
@@ -1,8 +1,17 @@
+import java.util.*;
+//Main fxn in class that calls upon Account and uses the Account.Java source code to do the grunt work.
 public class FirstBank {
     public static void main(String[] args) {
-        //TODO auto-generated method stub.
+        Scanner scan = new Scanner(System.in);
 
-        Account acc = new Account("Lorem Ipsum", "A00001");
+        System.out.println("Hello! Please input your account name and account ID to continue.");
+        System.out.print("Name: ");
+        String accName = scan.nextLine();
+        System.out.print("\nAccount ID: ");
+        String accID = scan.nextLine();
+        scan.close();
+        
+        Account acc = new Account(accName, accID);
         acc.showMenu();
     }
 }
